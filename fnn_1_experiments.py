473e4007795d38702fca4fd687c8fb7f2cc9a5d1
Erik Sennema <15833849+eriksennema@users.noreply.github.com>
2021-04-10 15:38:01 +0200

    change threads to 16

--- fnn_1_experiments.py
+++ fnn_1_experiments.py
@@ -4,7 +4,7 @@ import torch
 
 if __name__ == '__main__':
     # Check your computer by calling this in your regular python console: import torch; torch.get_num_threads()
-    torch.set_num_threads(2)
+    torch.set_num_threads(16)
     # Running Warehouse GRU 3 times with command:
     args = "--env-name Warehouse --yaml-file FNN --fnn-hidden-sizes 640,256 --algo ppo --use-gae --lr 2.5e-4 --clip-param 0.1 --value-loss-coef 1 --num-processes 16 --num-steps 8 --num-mini-batch 8 --log-interval 100 --use-linear-lr-decay --entropy-coef 0.01"
     arg_list = args.split(" ")
