83edabfea89931a48910d4f3f2c9ded25820a083
GijsKoning <gijs-koning@live.nl>
2021-04-14 13:42:51 +0200

    Add summary

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torchsummary import summary
 
 from a2c_ppo_acktr import algo, utils
 from a2c_ppo_acktr.algo import gail
@@ -44,6 +45,7 @@ def create_IAM_model(envs, args, parameters):
         action_space=envs.action_space,
         IAM=parameters['influence'],
         base_kwargs=base_kwargs)
+
     return actor_critic
 
 
@@ -98,6 +100,9 @@ class Main:
         # actor_critic = create_default_model(envs, args)
         actor_critic = create_IAM_model(envs, args, self.config_parameters)
         actor_critic.to(device)
+        # summary of the network
+        summary(actor_critic.base, [envs.observation_space.shape, (actor_critic.recurrent_hidden_state_size,), (args.num_processes,)], device='cpu')  # (in_channels, height, width)
+
         if args.algo == 'a2c':
             agent = algo.A2C_ACKTR(
                 actor_critic,
